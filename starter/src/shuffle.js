a59c68107d4505bae28715a9b19827427564517c
Yousef Sami <yousef28s10@gmail.com>
2025-06-26 21:08:48 +0300

    ESLint Modifications

--- starter/src/shuffle.js
+++ starter/src/shuffle.js
@@ -1,20 +1,25 @@
 //TODO
 //Note: You will not be able to run parcel without first fixing these errors
-//Note: Complete TODO in utilityRenderFunctions.js first. 
+//Note: Complete TODO in utilityRenderFunctions.js first.
 // Check for ESLint errors and format with Prettier.
 // This function is responsible for shuffling the flashcards.
 // While the function is mostly correct, there are some errors.
 // Use ESLint to identify the errors and Prettier to format the code.
 const shuffle = (cards) => {
-  var current = cards.length
-  var newCardsArray = [...cards
-  while (current != 0) {
-    le randomIndex = Math.floor(Math.random() * current)
-    current--
+  let current = cards.length;
+  const newCardsArray = [...cards];
 
-    [newCardsArray[current], newCardsArray[randomIndex]] = [newCardsArray[randomIndex],newCardsArray[current],]
+  while (current !== 0) {
+    const randomIndex = Math.floor(Math.random() * current);
+    current--;
+
+    [newCardsArray[current], newCardsArray[randomIndex]] = [
+      newCardsArray[randomIndex],
+      newCardsArray[current],
+    ];
   }
-  return newCardsArray
-}
 
-export { shuffle }
+  return newCardsArray;
+};
+
+export { shuffle };
