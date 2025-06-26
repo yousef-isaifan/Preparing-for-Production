358ce269773335e7059e56cf1b805d7b867ab4f8
Yousef Sami <yousef28s10@gmail.com>
2025-06-26 22:02:44 +0300

    Gulp file

---
+++ starter/gulpfile.js
@@ -0,0 +1,8 @@
+import gulp from "gulp";
+import shell from "gulp-shell";
+
+gulp.task("default", shell.task(["parcel index.html"]));
+
+gulp.task("test", shell.task(["npx mocha"]));
+
+gulp.task("e2e", shell.task(["npx cypress run"]));
