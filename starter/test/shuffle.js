956de8e872a52321b2463ec9e506cc0240269cd3
Yousef Sami <yousef28s10@gmail.com>
2025-06-26 22:03:33 +0300

    Package updates

---
+++ starter/test/shuffle.js
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import { shuffle } from "../src/shuffle.js";
+
+describe("shuffle function", () => {
+  it("should shuffle the array into a different order", () => {
+    const original = [1, 2, 3, 4, 5];
+    const result = shuffle(original);
+    expect(result).to.have.members(original);
+    expect(result).to.not.deep.equal(original);
+  });
+});
